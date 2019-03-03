4ca228afac1f1d8ad08290e55313374c7cfa5239
adamcsanki <adam.csanki.91@gmail.com>
2019-03-03 13:17:33 +0100

    finished

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ function Window_Load_Handler() {
 
     var nodeClickPrintButton = document.getElementById('printGrades');
     nodeClickPrintButton.addEventListener('click', PrintGrades_Click_Handler, false);
-
 }
 
 /*var picsagomb = document.getElementById('lofasz');
@@ -36,7 +35,7 @@ function AdatSor(Tanulo, Tantargy, Erdemjegy, Datum) {
     this.Datum = Datum;
 }
 
-var nevek = ['A Péter', 'O Rafael', 'Cs Ádám', 'P Imre'];
+var nevek = ['A. Péter', 'O. Rafael', 'Cs. Ádám', 'P. Imre'];
 var tantargyak = ['Biológia', 'Ének', 'Fizika', 'Földrajz', 'Matematika', 'Rajz', 'Történelem'];
 var jegyek = [5, 4, 3, 2, 1];
 var dataStore = [];
@@ -44,21 +43,21 @@ function nev_Generator() {
     var randomSzam = Math.floor(Math.random() * nevek.length)
     return nevek[randomSzam];
 }
-console.log(nev_Generator());
+
 
 function nev_tantargy_Generator() {
     var randomSzam = Math.floor(Math.random() * tantargyak.length);
     return tantargyak[randomSzam];
 }
 
-console.log(nev_tantargy_Generator());
+
 // jegyek generálása
 function jegy_Generator() {
     var randomSzam = Math.floor(Math.random() * jegyek.length);
     return jegyek[randomSzam];
 }
 
-console.log(jegy_Generator());
+
 
 function randomDate(date1, date2) {
     function getRandomArbitrary(min, max) {
@@ -75,19 +74,19 @@ function randomDate(date1, date2) {
 
     }
 }
-console.log(randomDate('09-01-2018', '01-31-2019'));
+
 
 function dataStoreGenerator() {
-    for (var i = 0; i < 101; i++) {
+    for (var i = 0; i < 200; i++) {
         dataStore.push(new AdatSor(nev_Generator(), nev_tantargy_Generator(), jegy_Generator(), randomDate('09-01-2018', '01-31-2019')));
     }
 
 }
 function LoadAdatsorokFromDatastore() {
     var temp = JSON.parse(jsonDataStore);
-    console.log(temp);
+    //console.log(temp);
     dataStore = temp.map(AnonymousAdatSorToAdatSor);
-    console.log(dataStore);
+    //console.log(dataStore);
 }
 
 function AnonymousAdatSorToAdatSor(item, index, originalArray) {
@@ -98,11 +97,6 @@ function AnonymousAdatSorToAdatSor(item, index, originalArray) {
         item.Datum
     );
 }
-/*function drawDatastore()
-{
-    var nodeDatastorePanel = document.querySelector('#pnlDatastore PRE');
-    nodeDatastorePanel.innerText = JSON.stringify(dataStore, null, 4)
-}*/
 
 function drawTable() {
     var nodeTBody = document.querySelector('#pnlTable TABLE > TBODY');
@@ -123,8 +117,6 @@ function drawTable() {
         resultTR.appendChild(TD3);
         resultTR.appendChild(TD4);
         nodeTBody.appendChild(resultTR);
-
-
     }
     return resultTR;
 }
@@ -133,7 +125,7 @@ console.log(JSON.stringify(dataStore));*/
 
 function fx() {
     var result = [];
-  
+
     for (var i = 0; i < nevek.length; i++) {
         for (var j = 0; j < tantargyak.length; j++) {
             var temp = dataStore.filter((item) => { return item.Tanulo === nevek[i] && item.Tantargy === tantargyak[j] });
@@ -141,12 +133,7 @@ function fx() {
             for (var k = 0; k < temp.length; k++) {
                 sum += temp[k].Erdemjegy;
             }
-            
-            
-            
-            //var atlag = sum / kabbe;
-            result.push({ Tanulo: nevek[i], Tantargy: tantargyak[j], Atlag: sum/temp.length });
-            //console.log(temp);
+            result.push({ Tanulo: nevek[i], Tantargy: tantargyak[j], Atlag: sum / temp.length });
         }
     }
     //console.log(JSON.stringify(result));
@@ -162,113 +149,85 @@ function drawAtlagTable(studentObjektum) {
         var resultTR = document.createElement('TR');
         var TD1 = document.createElement('TD');
         var TD2 = document.createElement('TD');
-        
-        
         TD1.innerText = studentObjektum[i].Tantargy;
         TD2.innerText = studentObjektum[i].Atlag.toFixed(1);
         resultTR.appendChild(TD1);
         resultTR.appendChild(TD2);
-        
         nodeTBody.appendChild(resultTR);
-        
-
-
     }
     return resultTR;
 }
 
-function Student1 ()
-{
+function Student1() {
     var result = [];
-    for (var i = 0; i< fx().length; i++)
-    {
-        if (fx()[i].Tanulo.indexOf('A Péter')>-1)
-        {
+    for (var i = 0; i < fx().length; i++) {
+        if (fx()[i].Tanulo.indexOf('A. Péter') > -1) {
             result.push(fx()[i]);
         }
     }
-    return result;
-}
-function Student2 ()
-{
-    var result = [];
-    for (var i = 0; i< fx().length; i++)
-    {
-        if (fx()[i].Tanulo.indexOf('O Rafael')>-1)
-        {
-            result.push(fx()[i]);
-        }
-    }
-    return result;
-}
-function Student3 ()
-{
-    var result = [];
-    for (var i = 0; i< fx().length; i++)
-    {
-        if (fx()[i].Tanulo.indexOf('Cs Ádám')>-1)
-        {
-            result.push(fx()[i]);
-        }
-    }
-    return result;
-}
-function Student4 ()
-{
-    var result = [];
-    for (var i = 0; i< fx().length; i++)
-    {
-        if (fx()[i].Tanulo.indexOf('P Imre')>-1)
-        {
-            result.push(fx()[i]);
-        }
-    }
-    
     return result;
 }
 
-function ChooseFilter(number)
-{
-    /*if (number == 1)
-    {
-        var chosenFilter = WholeClassFilter();
-    }*/
-    if (number ==2)
-    {
+function Student2() {
+    var result = [];
+    for (var i = 0; i < fx().length; i++) {
+        if (fx()[i].Tanulo.indexOf('O. Rafael') > -1) {
+            result.push(fx()[i]);
+        }
+    }
+    return result;
+}
+
+function Student3() {
+    var result = [];
+    for (var i = 0; i < fx().length; i++) {
+        if (fx()[i].Tanulo.indexOf('Cs. Ádám') > -1) {
+            result.push(fx()[i]);
+        }
+    }
+    return result;
+}
+
+function Student4() {
+    var result = [];
+    for (var i = 0; i < fx().length; i++) {
+        if (fx()[i].Tanulo.indexOf('P. Imre') > -1) {
+            result.push(fx()[i]);
+        }
+    }
+
+    return result;
+}
+
+function ChooseFilter(number) {
+    if (number == 2) {
         var chosenFilter = Student1();
     }
-    else if (number == 3)
-    {
+    else if (number == 3) {
         var chosenFilter = Student2();
     }
-    else if (number == 4)
-    {
+    else if (number == 4) {
         var chosenFilter = Student3();
     }
-    else if (number == 5)
-    {
+    else if (number == 5) {
         var chosenFilter = Student4();
     }
     return chosenFilter;
-}  
+}
 
-function StudentFilter_Click_Handler()
-{
+function StudentFilter_Click_Handler() {
     var filterSelect = document.getElementById('selectTanulo');
     var kategoria = parseInt(filterSelect.value);
-    var filterText = fx().Tanulo; 
-     
+    var filterText = fx().Tanulo;
+
     var filteredStudent = ChooseFilter(kategoria, filterText);
     var nodeTBodyAtlag = document.getElementById('pnlAtlag');
     drawAtlagTable(filteredStudent);
-
 }
 
-function drawPrintTable(studentObjektum)
-{
+function drawPrintTable(studentObjektum) {
     var nodePrintSection = document.getElementById('printTable');
     var resultDIV = document.createElement('DIV');
-    
     var resultTable = document.createElement('TABLE');
     var resultTHead = document.createElement('THEAD');
     var resultTRinTHead = document.createElement('TR');
@@ -278,8 +237,7 @@ function drawPrintTable(studentObjektum)
     var resultTH3inTHead = document.createElement('TH');
     var resultTFoot = document.createElement('TFOOT');
     var resultTBody = document.createElement('TBODY');
-    for (var i = 0; i<studentObjektum.length;i++)
-    {
+    for (var i = 0; i < studentObjektum.length; i++) {
         var erdemjegy = parseInt(studentObjektum[i].Atlag.toFixed(0));
         var resultTR = document.createElement('TR');
         var resultTD1 = document.createElement('TD');
@@ -308,34 +266,30 @@ function drawPrintTable(studentObjektum)
     }
 }
 
-function TanuloSzovegesErtekelese(erdemjegy)
-{
+function TanuloSzovegesErtekelese(erdemjegy) {
     var SzovegesErtekeles;
 
-    switch(erdemjegy)
-    {
+    switch (erdemjegy) {
         case 1:
-        SzovegesErtekeles='Elégtelen (1)';
-        break;
+            SzovegesErtekeles = 'Elégtelen (1)';
+            break;
         case 2:
-        SzovegesErtekeles='Elégséges (2)';
-        break;
+            SzovegesErtekeles = 'Elégséges (2)';
+            break;
         case 3:
-        SzovegesErtekeles='Közepes (3)';
-        break;
+            SzovegesErtekeles = 'Közepes (3)';
+            break;
         case 4:
-        SzovegesErtekeles='Jó (4)';
-        break;
+            SzovegesErtekeles = 'Jó (4)';
+            break;
         case 5:
-        SzovegesErtekeles='Jeles (5)';
-        break;
-        
+            SzovegesErtekeles = 'Jeles (5)';
+            break;
     }
     return SzovegesErtekeles;
 }
 
-function PrintTable_Click_Handler()
-{
+function PrintTable_Click_Handler() {
     var nodeClickGenerateButton = document.getElementById('printTable');
     nodeClickGenerateButton.innerText = '';
     drawPrintTable(Student1());
@@ -344,20 +298,7 @@ function PrintTable_Click_Handler()
     drawPrintTable(Student4());
 }
 
-function PrintGrades_Click_Handler()
-{
+function PrintGrades_Click_Handler() {
     window.print();
 
 }
-
-
-
-/*function TanuloFilter_Change_Handler()
-{
-    var filterSelect = document.getElementById('selectTanulo');
-    var filterValue = parseInt(filterSelect.value);
-    var filterDone = ChooseFilter(filterValue);
-    drawAtlagTable(filterDone);
-}*/
-
-
